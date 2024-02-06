282d13cb161f96711667a540c73a848c1b6893cc
xwhoknowz <jeremiah2498@gmail.com>
2024-02-05 20:07:13 -0600

    finished e-com backend

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,25 +4,41 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const products = await Product.findAll({
+      include: [{model: Tag, through: ProductTag}, {model: Category}]
+    })
+    res.status(200).json(products)
+  } catch (error) {
+    res.status(500).json(error)
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const product = await Product.findByPk(req.params.id, {
+      include: [{model: Tag, through: ProductTag}, {model: Category}]
+    })
+    res.status(200).json(product)
+  } catch (error) {
+    res.status(500).json(error)
+  }
 });
 
 // create new product
 router.post('/', (req, res) => {
   /* req.body should look like this...
     {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
+      "product_name": "Basketball",
+      "price": 200.00,
+      "stock": 3,
+      "tagIds": [1, 2, 3, 4]
     }
   */
   Product.create(req.body)
@@ -92,8 +108,18 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const delProduct = await Product.destroy({
+      where: {
+        id: req.params.id
+      },
+    });
+    res.status(200).json(delProduct)
+  } catch (error) {
+    res.status(500).json(error)
+  }
 });
 
 module.exports = router;
